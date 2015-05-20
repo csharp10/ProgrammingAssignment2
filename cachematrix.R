518382db825c94a461e7986710c5cf98c796fbd9
csharp10 <christopher.sharp8@gmail.com>
2015-05-19 20:47:49 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions cache and invert a selected matrix
 
-## Write a short comment describing this function
+## This function caches a matrix and inverts it.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  ##This step stores the sum of the head of the matrix to compare 
+  ##later to cacheSolve
+  ## cacheRan<<-head(sum(x))
+  
+  ##This creates a variable to cache the matrix as and one for the inverse
+  B<<-matrix()
+  Binv<<-matrix()
+  
+  ##This caches the matrix and it's inverse
+  B<<-A
+  Binv<<-solve(A)
 }
 
 
-## Write a short comment describing this function
+## This solves the matrix for it's inverse, OR, 
+##IF it is the same matrix previously used 
+##it supplys the previously calculated inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  if(B - x == 0) { 
+    Binv<<-solve(x) 
+    Binv}
+  else {"Matrix not cached"
+        Binv}
+  ## Return a matrix that is the inverse of 'x'
 }
